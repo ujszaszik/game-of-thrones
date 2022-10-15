45933487390fa9da92eec8cac6c2d6c365f3b70a
ujszaszik <ujszaszi.karesz@gmail.com>
2022-10-15 16:05:26 +0200

    Consume flows in Lifecycle-Safe manner

--- core/extension/src/main/java/hu/ujszaszik/extension/FlowExtensions.kt
+++ core/extension/src/main/java/hu/ujszaszik/extension/FlowExtensions.kt
@@ -1,11 +1,18 @@
 package hu.ujszaszik.extension
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun ViewModel.launch(block: suspend () -> Unit) {
     viewModelScope.launch { block() }
@@ -13,5 +20,23 @@ fun ViewModel.launch(block: suspend () -> Unit) {
 
 @Composable
 fun <T> Flow<T>.collectAsStateValue(): T? {
-    return collectAsState(initial = null).value
+    return collectAsStateWithLifecycle(initialValue = null).value
+}
+
+@Composable
+fun <T> Flow<T>.collectAsStateWithLifecycle(
+    initialValue: T,
+    lifecycle: Lifecycle = LocalLifecycleOwner.current.lifecycle,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    context: CoroutineContext = EmptyCoroutineContext
+): State<T> {
+    return produceState(initialValue, this, lifecycle, minActiveState, context) {
+        lifecycle.repeatOnLifecycle(minActiveState) {
+            if (context == EmptyCoroutineContext) {
+                this@collectAsStateWithLifecycle.collect { this@produceState.value = it }
+            } else withContext(context) {
+                this@collectAsStateWithLifecycle.collect { this@produceState.value = it }
+            }
+        }
+    }
 }
